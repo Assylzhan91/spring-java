9249ac96e0f1ef836a264f60c994991f23e2e33e
Асылжан Баймуратов <assylzhan.baimuratov@dmms.kz>
2025-09-30 21:44:43 +0500

    lesson spring-core-06

--- src/main/java/kz/spring/assylzhan/MusicPlayer.java
+++ src/main/java/kz/spring/assylzhan/MusicPlayer.java
@@ -4,12 +4,36 @@ package kz.spring.assylzhan;
  * @Assylzhan Baimuratov
  **/
 public class MusicPlayer {
-    private final Music music;
+    private Music music;
+    private String songName;
+    private int volume;
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(final String songName) {
+        this.songName = songName;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(final int volume) {
+        this.volume = volume;
+    }
+
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() {}
+
     public void playMusic() {
         System.out.println("Playing: " + music.getSong());
+    }
+    public void setMusic(Music music) {
+        this.music = music;
     }
 }
